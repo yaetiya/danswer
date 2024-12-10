98b0e765bfa2fea2cfef6db66948bb7c3629ee74
Michael Egorov <yaetiya@gmail.com>
2024-12-10 08:54:20 +0700

    - default application name

--- backend/ee/danswer/server/enterprise_settings/models.py
+++ backend/ee/danswer/server/enterprise_settings/models.py
@@ -28,7 +28,7 @@ class EnterpriseSettings(BaseModel):
 
     NOTE: don't put anything sensitive in here, as this is accessible without auth."""
 
-    application_name: str | None = "Search"
+    application_name: str | None = None
     use_custom_logo: bool = False
     use_custom_logotype: bool = False
 
